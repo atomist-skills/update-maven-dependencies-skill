26c6537b01d5645dd7efaf6498f2bfef88bda429
Jim Clark <slimslenderslacks@gmail.com>
2020-01-23 16:31:52 -0800

    initial structure commit

---
+++ dev/user.cljs
@@ -0,0 +1,7 @@
+(ns user
+  (:require [atomist.main]
+            [atomist.cljs-log :as log]))
+
+(enable-console-print!)
+
+(def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_PROD))
---
+++ src/atomist/main.cljs
@@ -0,0 +1,41 @@
+(ns atomist.main
+  (:require [cljs.pprint :refer [pprint]]
+            [cljs.core.async :refer [<! timeout chan]]
+            [clojure.string :as s]
+            [goog.crypt.base64 :as b64]
+            [goog.string :as gstring]
+            [goog.string.format]
+            [atomist.cljs-log :as log]
+            [atomist.editors :as editors]
+            [atomist.sdmprojectmodel :as sdm]
+            [atomist.json :as json]
+            [atomist.api :as api]
+            [atomist.promise :as promise])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn middleware [handler]
+  (fn [request]
+    (handler request)))
+
+(defn process-request
+  "process the request pipeline for any events arriving in this skill"
+  [request]
+  (let [done-channel (chan)]
+    ;; create a pipeline of handlers but always end by writing to the done channel and logging something
+    ((-> (fn [ch-request]
+           (log/info "----> finished")
+           (go (>! (:done-channel ch-request) :done)))
+         (middleware)) (assoc request
+             :done-channel done-channel))
+    done-channel))
+
+(defn ^:export handler
+  "handler
+    must return a Promise - we don't do anything with the value
+    params
+      data - Incoming Request #js object
+      sendreponse - callback ([obj]) puts an outgoing message on the response topic"
+  [data sendreponse]
+  (promise/chan->promise
+   (process-request (assoc (js->clj data :keywordize-keys true)
+                      :sendreponse sendreponse))))
