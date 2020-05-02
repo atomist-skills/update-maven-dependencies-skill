4fe2b9857e95b20b11540951c0f6cc0b5f54b6c2
Jim Clark <slimslenderslacks@gmail.com>
2020-05-02 00:50:19 -0700

    Bump update-maven-dependencies-skill to 0.2.13 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "update-maven-dependencies-skill")
 (def path ".")
-(def version "0.2.12")
-(def tag "v0.2.12")
-(def generated-at "2020-05-01T02:16:56Z")
+(def version "0.2.13")
+(def tag "v0.2.13")
+(def generated-at "2020-05-02T07:50:19Z")
